54d831e03aba4f5bd9c7a98ebcf33f6b79976439
mervebduman <merveb.duman@hotmail.com>
2024-12-05 20:45:23 +0300

    qc script and results

---
+++ scripts/qc.R
@@ -0,0 +1,24 @@
+# Setup
+library(Rqc)
+library(fastqcr)
+fastqc_install()
+
+# Load files
+folder = "./data/"
+qcRes=rqc(path = folder, pattern = ".fastq.gz", openBrowser=FALSE)
+
+# Sequence quality per base/cycle
+rqcCycleQualityBoxPlot(qcRes)
+
+# Sequence content per base/cycle
+rqcCycleBaseCallsLinePlot(qcRes)
+
+# Read frequency plot
+rqcReadFrequencyPlot(qcRes)
+
+# Call FASTQC and record the results
+fastqc(fq.dir = folder, qc.dir = "./results/fastqc_results")
+
+# View the report rendered by R functions
+qc_report(qc.path="./results/fastqc_results", 
+          result.file="reportFile", preview = TRUE)
